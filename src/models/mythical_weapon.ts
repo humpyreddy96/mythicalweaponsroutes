6e3a9078a0cd28f77a2ae5a5a1a985537eb43ae9
humpyreddy96 <hp411819@ohio.edu>
2021-07-19 19:19:08 -0400

    initial commit

--- src/models/mythical_weapon.ts
+++ src/models/mythical_weapon.ts
@@ -14,7 +14,7 @@ export class MythicalWeaponStore{
         try{
             // @ts-ignore
             const conn = await client.connect() 
-            const sql = 'SELECT * FROM mythical_weapons'
+            const sql = 'SELECT * FROM mythical_weapons' 
             const result = await conn.query(sql)
             conn.release()
             return result.rows
