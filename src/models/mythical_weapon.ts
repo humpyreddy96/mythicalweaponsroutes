1ce38eba06aa5b43e56edcc859d6750062c167e0
humpyreddy96 <hp411819@ohio.edu>
2021-07-19 18:42:21 -0400

    first commit

---
+++ src/models/mythical_weapon.ts
@@ -0,0 +1,28 @@
+import client from '../database'
+
+export type Weapon = {
+    id:Number;
+    name:string;
+    type:string;
+    weight:number;
+}
+
+export class MythicalWeaponStore{
+
+    async index():Promise<Weapon[]>{
+
+        try{
+            // @ts-ignore
+            const conn = await client.connect() 
+            const sql = 'SELECT * FROM mythical_weapons'
+            const result = await conn.query(sql)
+            conn.release()
+            return result.rows
+    
+        }catch(err){
+            throw new Error(`Cannot get wepaons ${err}`)
+        }
+       
+    }
+}
+
