0749af831779ceb7a1ade3e86fa3d12ff7ae6815
humpyreddy96 <hp411819@ohio.edu>
2021-07-23 09:33:24 -0400

    trying to update user details

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,6 +1,7 @@
 import express, {request, Request,Response} from 'express'
 import {User,Users} from '../models/user'
 import jwt from 'jsonwebtoken'
+import verifyAuthToken from './verifyAuthToken'
 
 
 const user_data = new Users()
@@ -45,10 +46,40 @@ const authenticate = async(req:Request,res:Response)=>{
     }
 }
 
+const update = async (req: Request, res: Response) => {
+    const user: User = {
+        id: parseInt(req.params.id),
+        username: req.body.username,
+        password: req.body.password,
+    }
+    try {
+        const authorizationHeader = req.headers.authorization!
+        const token = authorizationHeader.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET!)
+        //@ts-ignore
+        if(decoded.id !== user.id) {
+            throw new Error('User id does not match!')
+        }
+    } catch(err) {
+        res.status(401)
+        res.json(err)
+        return
+    }
+
+    try {
+        const updated = await user_data.create(user)
+        res.json(updated)
+    } catch(err) {
+        res.status(400)
+        res.json(err + user)
+    }
+}
+
 const users_routes = (app:express.Application) =>{
     app.get('/users',index)
     app.post('/users',create)
     app.post('/users/authenticate',authenticate)
+    app.post('/users/:id',update)
     
 }
 
